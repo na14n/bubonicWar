ee85c9720a80a512c2c52c668c2ab5defdde96ed
KayL-1 <71209263+KayL-1@users.noreply.github.com>
2023-01-09 16:17:17 +0800

    Spawner
    very working spawner

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
-    public GameObject rat;
-    public GameObject bigRat;
-    public GameObject flea;
-    public GameObject bigFlea;
-    public float timetoSpawnSmall = 5f;
-    public float timetoSpawnBig = 10f;
-    float elapsedTimeSmall;
-    float elapsedTimeBig;
+        // Array to hold the wave information
+    public Wave[] waves;
 
     void Start()
     {
+        // Get the Spawner component on this GameObject
+        spawner spawner = GetComponent<spawner>();
+        // Start the spawner coroutine
+        StartCoroutine(spawner.StartSpawning());
     }
 
-    // Update is called once per frame
-    void Update()
+    // Function to spawn the monsters for a wave
+    public IEnumerator SpawnWave(Wave wave)
     {
+        for (int i = 0; i < wave.numMonsters; i++)
+        {
+            // Instantiate the monster prefab
+            GameObject monster = Instantiate(wave.monsterPrefab, transform.position, Quaternion.identity);
 
+            // Wait for the specified interval before spawning the next monster
+            yield return new WaitForSeconds(wave.spawnInterval);
+        }
     }
 
-
-        IEnumerator SpawnPrefabRat(float interval, GameObject enemy)
+    // Coroutine to control the spawner
+    public IEnumerator StartSpawning()
     {
-        yield return new WaitForSeconds(interval);
-        Instantiate(enemy, this.transform.position, Quaternion.identity);
-        StartCoroutine(SpawnPrefabRat(interval, enemy));
+        // Iterate through the waves in the array
+        for (int i = 0; i < waves.Length; i++)
+        {
+            // Spawn the monsters for the current wave
+            StartCoroutine(SpawnWave(waves[i]));
+
+            // Wait for the specified interval before moving on to the next wave
+            yield return new WaitForSeconds(waves[i].waveInterval);
+        }
     }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public GameObject monsterPrefab;
+        public int numMonsters;
+        public float spawnInterval;
+        public float waveInterval;
+    }
+    
 }
+
