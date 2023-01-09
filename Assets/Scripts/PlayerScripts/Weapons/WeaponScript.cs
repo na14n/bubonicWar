d511133f1a6f2e56c97368446228a5608968842f
KayL-1 <71209263+KayL-1@users.noreply.github.com>
2023-01-09 09:30:26 +0800

    last update before merge
    bam

--- Assets/Scripts/PlayerScripts/Weapons/WeaponScript.cs
+++ Assets/Scripts/PlayerScripts/Weapons/WeaponScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WeaponScript : MonoBehaviour
 {
         public int totalWeapons = 1;
-        public int weaponIndex = 2;
+        public int weaponIndex;
         
         public GameObject[] weapons;
         public GameObject weaponHolder;
@@ -14,49 +14,69 @@ public class WeaponScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        totalWeapons = weaponHolder.transform.childCount;
-        weapons = new GameObject[totalWeapons];
-
-        for (int i = 0; i < totalWeapons; i++)
-        {
-            weapons[i] = weaponHolder.transform.GetChild(i).gameObject;
-            weapons[i].SetActive(false);
-        }
-
-        weapons[0].SetActive(true);
-        currentWep = weapons[0];
-        weaponIndex = 0;
+        UpdateWeaponsArray();
     }
 
     // Update is called once per frame
     void Update()
+{
+if(Input.GetKeyDown(KeyCode.E))
+{
+    if (weaponIndex < 0 || weaponIndex >= totalWeapons)
     {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-        
-        if(weaponIndex < totalWeapons-1)
-        {
-            weapons[weaponIndex].SetActive(false);
-            weapons[weaponIndex].GetComponent<Collider2D>().enabled = false;
-            weaponIndex += 1;
-            weapons[weaponIndex].SetActive(true);
-            weapons[weaponIndex].GetComponent<Collider2D>().enabled = true;
-
-        }
-        }
-
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-        
-        if(weaponIndex > 0)
-        {
-            weapons[weaponIndex].SetActive(false);
-            weapons[weaponIndex].GetComponent<Collider2D>().enabled = false;
-            weaponIndex -= 1;
-            weapons[weaponIndex].SetActive(true);
-            weapons[weaponIndex].GetComponent<Collider2D>().enabled = true;
-
-        }
-        }
+        return;
     }
+    if(weaponIndex < totalWeapons-1)
+    {
+        int newIndex = weaponIndex + 1;
+        if (newIndex < 0 || newIndex >= totalWeapons)
+        {
+            return;
+        }
+        weapons[weaponIndex].SetActive(false);
+        weapons[weaponIndex].GetComponent<Collider2D>().enabled = false;
+
+        weapons[newIndex].SetActive(true);
+        weapons[newIndex].GetComponent<Collider2D>().enabled = true;
+        weaponIndex = newIndex;
+    }
+}
+
+if(Input.GetKeyDown(KeyCode.Q))
+{
+    if (weaponIndex < 0 || weaponIndex >= totalWeapons)
+    {
+        return;
+    }
+    if(weaponIndex > 0)
+    {
+        int newIndex = weaponIndex - 1;
+        if (newIndex < 0 || newIndex >= totalWeapons)
+        {
+            return;
+        }
+        weapons[weaponIndex].SetActive(false);
+        weapons[weaponIndex].GetComponent<Collider2D>().enabled = false;
+
+        weapons[newIndex].SetActive(true);
+        weapons[newIndex].GetComponent<Collider2D>().enabled = true;
+        weaponIndex = newIndex;
+    }
+}
+}
+
+public void UpdateWeaponsArray()
+{
+    totalWeapons = weaponHolder.transform.childCount;
+    weapons = new GameObject[totalWeapons];
+    for (int i = 0; i < totalWeapons; i++)
+    {
+        weapons[i] = weaponHolder.transform.GetChild(i).gameObject;
+        weapons[i].SetActive(false);
+    }
+
+    weapons[0].SetActive(true);
+    currentWep = weapons[0];
+    weaponIndex = 0;
+}
 }
--- Assets/Scripts/PlayerScripts/playerStats.cs
+++ Assets/Scripts/PlayerScripts/playerStats.cs
@@ -9,10 +9,17 @@ public class playerStats : MonoBehaviour
     public float maxHP = 100;
     public int xp;
     public float maxXP = 100f;
-    public float playerLvl = 5;
+    public float playerLvl = 1;
     public float speed = 5f;
     public GameObject lvlUpPanel;
     public Enemy enemyStats;
+
+    public GameObject wepPrefab1;
+    public GameObject wepPrefab2;
+    public GameObject wepPrefab3;
+    public GameObject wepPrefab4;
+    // bool wepUnlocked = false;
+    // public GameObject wepHandler;
     void Start()
     {
         this.GetComponent<health>().setHealth(hp,maxHP);
@@ -39,10 +46,11 @@ public class playerStats : MonoBehaviour
     public void lvlUp()
     {
             if (xp >= maxXP)
-        {
+        {   
             playerLvl = playerLvl + 1;
             maxXP = maxXP + 150 * 1.5f;
             lvlUpPanel.SetActive(true);
+            // unlockWep();
             Time.timeScale = 0;
         }
     }
@@ -67,4 +75,53 @@ public class playerStats : MonoBehaviour
 
     }
 
+// public void unlockWep()
+// {
+//     if (!wepUnlocked)
+//     {   
+//         Debug.Log("lvl up");
+//         if (playerLvl == 3)
+//         {   
+//             Debug.Log("weapon unlocked");
+//             GameObject warhammer = Instantiate(wepPrefab1, wepHandler.transform);
+//             wepUnlocked = true;
+//             WeaponScript weaponScript = wepHandler.GetComponent<WeaponScript>();
+//             weaponScript.weapons[weaponScript.totalWeapons] = warhammer;
+//             weaponScript.totalWeapons++;
+//             weaponScript.UpdateWeaponsArray();
+//             warhammer.SetActive(false);
+//         }
+
+//         if (playerLvl == 6)
+//         {
+//             GameObject spear = Instantiate(wepPrefab2, wepHandler.transform);
+//             wepUnlocked = true;
+//             // Add the spear game object to the weapons array
+//             WeaponScript weaponScript = wepHandler.GetComponent<WeaponScript>();
+//             weaponScript.weapons[weaponScript.totalWeapons] = spear;
+//         }
+
+//         if (playerLvl == 9)
+//         {
+//             GameObject doubledagg = Instantiate(wepPrefab3, wepHandler.transform);
+//             wepUnlocked = true;
+//             // Add the doubledagg game object to the weapons array
+//             WeaponScript weaponScript = wepHandler.GetComponent<WeaponScript>();
+//             weaponScript.weapons[weaponScript.totalWeapons] = doubledagg;
+//             weaponScript.totalWeapons++;
+//         }
+
+//         if (playerLvl == 12)
+//         {
+//             GameObject axe = Instantiate(wepPrefab4, wepHandler.transform);
+//             wepUnlocked = true;
+//             // Add the axe game object to the weapons array
+//             WeaponScript weaponScript = wepHandler.GetComponent<WeaponScript>();
+//             weaponScript.weapons[weaponScript.totalWeapons] = axe;
+//             weaponScript.totalWeapons++;
+//         }
+//     }
+// }
+
 }
+
