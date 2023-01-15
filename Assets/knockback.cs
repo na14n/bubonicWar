a454cadb47e93a70f17bbf66d0d5769548521868
KayL-1 <71209263+KayL-1@users.noreply.github.com>
2023-01-15 11:07:22 +0800

    added few lines in knockback

--- Assets/knockback.cs
+++ Assets/knockback.cs
@@ -7,7 +7,7 @@ public class knockback : MonoBehaviour
     public float knockbackForce = 10.0f;
     public bool knockedBack = false;
     public Vector2 knockbackDirection;
-
+    private Vector2 lastKnockbackDirection;
     private Rigidbody2D rb2d;
 
     void Start()
@@ -17,18 +17,18 @@ public class knockback : MonoBehaviour
 
     void Update()
     {
-        if (knockedBack)
-        {
-            // Apply knockback force in the direction of the attacker
-            rb2d.AddForce(knockbackDirection * knockbackForce);
-        }
+
     }
 
     public void Knockback(Vector2 direction)
-    {
+    {   
+        if (direction != lastKnockbackDirection) {
         knockedBack = true;
         knockbackDirection = -(direction - new Vector2(this.transform.position.x, this.transform.position.y)).normalized;
+        rb2d.AddForce(knockbackDirection * knockbackForce);
         Invoke("offKnockBack", 0.5f);
+        Invoke("offKnockBack", 1f);
+        }
     }
 
     public void offKnockBack()
